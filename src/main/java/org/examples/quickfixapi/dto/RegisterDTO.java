d58b9622ab038393ef1a880061bd4ebff27d792f
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-23 22:15:05 +0530

    added constraints for non-blank, email format, password length, and non-null role

--- src/main/java/org/examples/quickfixapi/dto/RegisterDTO.java
+++ src/main/java/org/examples/quickfixapi/dto/RegisterDTO.java
@@ -1,5 +1,8 @@
 package org.examples.quickfixapi.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.examples.quickfixapi.entity.Role;
 
@@ -8,9 +11,18 @@ import org.examples.quickfixapi.entity.Role;
 @Data
 public class RegisterDTO {
 
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 40, message = "Password must be between 6 and 20 characters")
     private String password;
+
+    @NotBlank(message = "Role is required")
     private Role role;
 
 
