fdaeb84a633d5ff27e813bf063f8bcddd6c3de9b
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-23 22:07:27 +0530

    added email and password fields with jarakta validation for non-blank and email format

--- src/main/java/org/examples/quickfixapi/dto/AuthDTO.java
+++ src/main/java/org/examples/quickfixapi/dto/AuthDTO.java
@@ -1,12 +1,18 @@
 package org.examples.quickfixapi.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
 @Setter
 public class AuthDTO {
 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
 
 }
