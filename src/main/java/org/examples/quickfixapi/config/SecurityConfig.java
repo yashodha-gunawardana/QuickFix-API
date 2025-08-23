5d3ac889660bb664a3049a24ae4884f8275858a1
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-24 01:44:33 +0530

    add AuthenticationProvider bean using DaoAuthenticationProvider

--- src/main/java/org/examples/quickfixapi/config/SecurityConfig.java
+++ src/main/java/org/examples/quickfixapi/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.examples.quickfixapi.util.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -61,7 +62,12 @@ public class SecurityConfig {
         return source;
     }
 
-    private AuthenticationProvider authenticationProvider() {
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
     }
 
 }
