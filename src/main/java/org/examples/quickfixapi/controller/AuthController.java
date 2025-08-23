e0df5c56cd434bf720a36f17c7cd69ff1fe65194
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-24 02:19:22 +0530

    handles POST requests to /api/auth/register

--- src/main/java/org/examples/quickfixapi/controller/AuthController.java
+++ src/main/java/org/examples/quickfixapi/controller/AuthController.java
@@ -1,6 +1,28 @@
 package org.examples.quickfixapi.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.examples.quickfixapi.dto.ApiResponse;
+import org.examples.quickfixapi.dto.RegisterDTO;
+import org.examples.quickfixapi.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse> register(@Valid @RequestBody RegisterDTO registerDTO) {
+//        authService.register(registerDTO);
+        return ResponseEntity.ok(new ApiResponse(200, "User register successfully..!", authService.register(registerDTO)));
+    }
 
 
 }
