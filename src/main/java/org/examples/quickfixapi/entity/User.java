07068ec74476d6d8c88a248a0be06566546fa5df
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-23 22:35:41 +0530

    includes fields for id, username, email, password, role, and enabled status

--- src/main/java/org/examples/quickfixapi/entity/User.java
+++ src/main/java/org/examples/quickfixapi/entity/User.java
@@ -1,7 +1,24 @@
 package org.examples.quickfixapi.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
 public class User {
 
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private Role role;
+
+    private boolean enabled = true;
 
 }
