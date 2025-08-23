85c432f8ad000612df642281cf366f4f5e001da3
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-24 01:39:44 +0530

    create SecurityConfig base class with annotations and required dependencies

--- src/main/java/org/examples/quickfixapi/config/SecurityConfig.java
+++ src/main/java/org/examples/quickfixapi/config/SecurityConfig.java
@@ -1,6 +1,22 @@
 package org.examples.quickfixapi.config;
 
+import lombok.RequiredArgsConstructor;
+import org.examples.quickfixapi.util.JwtAuthFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
 
 
