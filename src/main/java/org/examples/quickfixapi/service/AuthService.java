b6f7e96677b21be6ea4eb35baff0a52d6c22cb51
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-24 01:53:07 +0530

    inject dependencies

--- src/main/java/org/examples/quickfixapi/service/AuthService.java
+++ src/main/java/org/examples/quickfixapi/service/AuthService.java
@@ -1,12 +1,21 @@
 package org.examples.quickfixapi.service;
 
 import lombok.RequiredArgsConstructor;
+import org.examples.quickfixapi.respository.UserRepository;
+import org.examples.quickfixapi.util.JwtUtil;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
 
 }
