44fcc84b93933bf4bb404200acab07a5f631a788
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-24 01:54:44 +0530

    add register() method for new users

--- src/main/java/org/examples/quickfixapi/respository/UserRepository.java
+++ src/main/java/org/examples/quickfixapi/respository/UserRepository.java
@@ -1,5 +1,7 @@
 package org.examples.quickfixapi.respository;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.examples.quickfixapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +12,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(@NotBlank(message = "Email is required") @Email(message = "Invalid email format") String email);
+
+    boolean existsByUsername(@NotBlank(message = "Username is required") String username);
 }
--- src/main/java/org/examples/quickfixapi/service/AuthService.java
+++ src/main/java/org/examples/quickfixapi/service/AuthService.java
@@ -1,6 +1,9 @@
 package org.examples.quickfixapi.service;
 
 import lombok.RequiredArgsConstructor;
+import org.examples.quickfixapi.dto.RegisterDTO;
+import org.examples.quickfixapi.entity.User;
+import org.examples.quickfixapi.exception.UserAlreadyExistsException;
 import org.examples.quickfixapi.respository.UserRepository;
 import org.examples.quickfixapi.util.JwtUtil;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,5 +20,23 @@ public class AuthService implements UserDetailsService {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
 
+    public String register(RegisterDTO registerDTO) {
+        if (userRepository.existsByEmail(registerDTO.getEmail())) {
+            throw new RuntimeException("Email already in use");
+        }
+        if (userRepository.existsByUsername(registerDTO.getUsername())) {
+            throw new UserAlreadyExistsException("Username already in use");
+        }
+
+        User user = User.builder()
+                .email(registerDTO.getEmail())
+                .password(passwordEncoder.encode(registerDTO.getPassword()))
+                .username(registerDTO.getUsername())
+                .role(registerDTO.getRole())
+                .enabled(true)
+                .build();
+        userRepository.save(user);
+        return "User registered successfully";
+    }
 
 }
