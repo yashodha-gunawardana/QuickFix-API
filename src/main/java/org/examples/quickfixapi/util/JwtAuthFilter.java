7089835df56b92423a19abdc5e6a0a24a4a73042
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-24 01:32:23 +0530

    extract authorization header and check token format

--- src/main/java/org/examples/quickfixapi/util/JwtAuthFilter.java
+++ src/main/java/org/examples/quickfixapi/util/JwtAuthFilter.java
@@ -1,14 +1,26 @@
 package org.examples.quickfixapi.util;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+
 @Component
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
 
-        private final JwtUtil jwtUtil;
-        private final UserDetailsService userDetailsService;
-}
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+    }
