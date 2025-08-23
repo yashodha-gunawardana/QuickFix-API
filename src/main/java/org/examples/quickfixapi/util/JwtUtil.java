11f34b27c2812204179e041589161e7409346b1a
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-24 01:13:15 +0530

    create JwtUtil base class with secret key and expiration setup

--- src/main/java/org/examples/quickfixapi/util/JwtUtil.java
+++ src/main/java/org/examples/quickfixapi/util/JwtUtil.java
@@ -1,7 +1,20 @@
 package org.examples.quickfixapi.util;
 
+import io.jsonwebtoken.security.Keys;
+import lombok.Value;
+import org.springframework.stereotype.Component;
 
+import java.security.Key;
+
+@Component
 public class JwtUtil {
 
+    private final Key key;
+    private final long expiration;
 
+    public JwtUtil(@Value("${jwt.secretKey}") String secretKey,
+                   @Value("${jwt.expiration}") long expiration) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        this.expiration = expiration;
+    }
 }
