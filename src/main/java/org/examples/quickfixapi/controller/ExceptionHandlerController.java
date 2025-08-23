d191afcdd6b87fc99b6c3aafc724beed6af16d28
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-24 02:01:16 +0530

    create ExceptionHandlerController class

--- src/main/java/org/examples/quickfixapi/controller/ExceptionHandlerController.java
+++ src/main/java/org/examples/quickfixapi/controller/ExceptionHandlerController.java
@@ -1,5 +1,8 @@
 package org.examples.quickfixapi.controller;
 
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class ExceptionHandlerController {
 
 
