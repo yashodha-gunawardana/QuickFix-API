d441df11024a5e1274224467962546d179336ea2
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-24 00:33:47 +0530

    create ApplicationConfig base class with @Configuration

--- src/main/java/org/examples/quickfixapi/config/ApplicationConfig.java
+++ src/main/java/org/examples/quickfixapi/config/ApplicationConfig.java
@@ -1,7 +1,10 @@
 package org.examples.quickfixapi.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
 
 
