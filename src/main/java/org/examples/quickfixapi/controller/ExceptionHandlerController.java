c64991b1b81a347d4605f107cd4212038f972d84
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-24 02:02:03 +0530

    handle UserAlreadyExistsException

--- src/main/java/org/examples/quickfixapi/controller/ExceptionHandlerController.java
+++ src/main/java/org/examples/quickfixapi/controller/ExceptionHandlerController.java
@@ -1,9 +1,17 @@
 package org.examples.quickfixapi.controller;
 
+import org.examples.quickfixapi.exception.UserAlreadyExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionHandlerController {
 
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<String> handleUserAlreadyExists(UserAlreadyExistsException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
 
 }
