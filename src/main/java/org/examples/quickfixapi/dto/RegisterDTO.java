e6d647462747173549cad68b01dacb87358ae31f
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-23 22:10:10 +0530

    add RegisterDTO for user registration request

--- src/main/java/org/examples/quickfixapi/dto/RegisterDTO.java
+++ src/main/java/org/examples/quickfixapi/dto/RegisterDTO.java
@@ -1,7 +1,17 @@
 package org.examples.quickfixapi.dto;
 
+import lombok.*;
+import org.examples.quickfixapi.entity.Role;
+
+@Getter
+@Setter
+@Data
 public class RegisterDTO {
 
+    private String username;
+    private String email;
+    private String password;
+    private Role role;
 
 
 }
