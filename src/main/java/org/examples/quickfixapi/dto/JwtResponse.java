1f3a3f623f5f5fc9bec011bf4f726353515d77e0
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-23 22:18:34 +0530

    includes fields for token, role, and username with Lombok annotations for getters, setters, and constructor

--- src/main/java/org/examples/quickfixapi/dto/JwtResponse.java
+++ src/main/java/org/examples/quickfixapi/dto/JwtResponse.java
@@ -1,8 +1,14 @@
 package org.examples.quickfixapi.dto;
 
+import lombok.*;
 
-
+@Getter
+@Setter
+@AllArgsConstructor
 public class JwtResponse {
 
+    private String token;
+    private String role;
+    private String username;
 
 }
