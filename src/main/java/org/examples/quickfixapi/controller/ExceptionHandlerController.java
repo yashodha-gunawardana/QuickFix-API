ef61bbc302b99c6c1c3d71f98733caa3d8fc4ed5
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-24 02:03:18 +0530

    handle UsernameNotFoundException

--- src/main/java/org/examples/quickfixapi/controller/ExceptionHandlerController.java
+++ src/main/java/org/examples/quickfixapi/controller/ExceptionHandlerController.java
@@ -4,6 +4,7 @@ import org.examples.quickfixapi.exception.UserAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,6 +19,11 @@ public class ExceptionHandlerController {
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<String> handleBadCredentials(BadCredentialsException ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<String> handleUsernameNotFound(UsernameNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
 
