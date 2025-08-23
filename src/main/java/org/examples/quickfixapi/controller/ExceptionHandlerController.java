2be003caa65032aceae2187f93b09b13f20a657c
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-08-24 02:02:35 +0530

    handle BadCredentialsException

--- src/main/java/org/examples/quickfixapi/controller/ExceptionHandlerController.java
+++ src/main/java/org/examples/quickfixapi/controller/ExceptionHandlerController.java
@@ -3,6 +3,7 @@ package org.examples.quickfixapi.controller;
 import org.examples.quickfixapi.exception.UserAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +14,11 @@ public class ExceptionHandlerController {
     public ResponseEntity<String> handleUserAlreadyExists(UserAlreadyExistsException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentials(BadCredentialsException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+    }
+
 
 }
