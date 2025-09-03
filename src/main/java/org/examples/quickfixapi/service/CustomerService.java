c3cee027ac4922e88c959e107fda219a3b1784e1
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-03 15:25:20 +0530

    add CustomerService class with constructor injection for repositories

--- src/main/java/org/examples/quickfixapi/service/CustomerService.java
+++ src/main/java/org/examples/quickfixapi/service/CustomerService.java
@@ -1,6 +1,18 @@
 package org.examples.quickfixapi.service;
 
+import org.examples.quickfixapi.respository.ProviderRequestRepository;
+import org.examples.quickfixapi.respository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerService {
 
+    private final ProviderRequestRepository providerRequestRepository;
+    private final UserRepository userRepository;
+
+    public CustomerService(ProviderRequestRepository providerRequestRepository, UserRepository userRepository) {
+        this.providerRequestRepository = providerRequestRepository;
+        this.userRepository = userRepository;
+    }
 
 }
