0bdb29152c769f84cd7a78886812bf0942d8bfeb
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-03 15:01:09 +0530

    add getpendingRequests method to fetch pending provider requests

--- src/main/java/org/examples/quickfixapi/service/AdminService.java
+++ src/main/java/org/examples/quickfixapi/service/AdminService.java
@@ -1,8 +1,11 @@
 package org.examples.quickfixapi.service;
 
+import org.examples.quickfixapi.entity.ProviderRequest;
 import org.examples.quickfixapi.respository.ProviderRequestRepository;
 import org.examples.quickfixapi.respository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AdminService {
@@ -15,5 +18,11 @@ public class AdminService {
         this.userRepository = userRepository;
     }
 
+    public List<ProviderRequest> getPendingRequests() {
+        return providerRequestRepository.findAll()
+                .stream()
+                .filter(r -> r.getStatus().equals("PENDING"))
+                .toList();
+    }
 
 }
