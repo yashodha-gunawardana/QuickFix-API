c6798c3ad009dc9927eee484ab01d019ec2c1156
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-03 14:36:50 +0530

    add providerRequestDTO withh request details and metadata

--- src/main/java/org/examples/quickfixapi/dto/ProviderRequestDTO.java
+++ src/main/java/org/examples/quickfixapi/dto/ProviderRequestDTO.java
@@ -1,6 +1,19 @@
 package org.examples.quickfixapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
 public class ProviderRequestDTO {
 
-
+    private Long requestId;
+    private Long userId;
+    private String name;
+    private String currentRole;
+    private String status;
+    private LocalDateTime requestDate;
+    private String requestedRole;
 }
