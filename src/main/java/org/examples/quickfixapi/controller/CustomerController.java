49ddeeb8664ad065f09eef431209cd8d07e73dfb
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-03 15:28:00 +0530

    add CustomerController class with CustomerService injection

--- src/main/java/org/examples/quickfixapi/controller/CustomerController.java
+++ src/main/java/org/examples/quickfixapi/controller/CustomerController.java
@@ -1,7 +1,19 @@
 package org.examples.quickfixapi.controller;
 
+import org.examples.quickfixapi.service.CustomerService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/customer")
 public class CustomerController {
 
+    private final CustomerService customerService;
 
-
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 }
+
+
+
