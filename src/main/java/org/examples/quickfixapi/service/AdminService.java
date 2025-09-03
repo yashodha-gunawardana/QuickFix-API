720d1798eecfc860b82c0c8bda40b7576d9ddd75
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-03 14:56:11 +0530

    add adminService class with constructor injection for repositories

--- src/main/java/org/examples/quickfixapi/service/AdminService.java
+++ src/main/java/org/examples/quickfixapi/service/AdminService.java
@@ -1,7 +1,19 @@
 package org.examples.quickfixapi.service;
 
+import org.examples.quickfixapi.respository.ProviderRequestRepository;
+import org.examples.quickfixapi.respository.UserRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AdminService {
+
+    private final ProviderRequestRepository providerRequestRepository;
+    private final UserRepository userRepository;
+
+    public AdminService(ProviderRequestRepository providerRequestRepository, UserRepository userRepository) {
+        this.providerRequestRepository = providerRequestRepository;
+        this.userRepository = userRepository;
+    }
 
 
 }
