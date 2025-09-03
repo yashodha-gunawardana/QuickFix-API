c4eafc6c98c3044337ef86917be9f44492d36184
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-03 15:28:53 +0530

    add POST endpoint /request-provider to create provider request for customer

--- src/main/java/org/examples/quickfixapi/controller/CustomerController.java
+++ src/main/java/org/examples/quickfixapi/controller/CustomerController.java
@@ -1,8 +1,12 @@
 package org.examples.quickfixapi.controller;
 
 import org.examples.quickfixapi.service.CustomerService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/customer")
@@ -13,6 +17,14 @@ public class CustomerController {
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
+
+    @PostMapping("/request-provider")
+    public Map<String, String> requestProvider(@RequestBody Map<String, Long> body) {
+        Long userId = body.get("userId");
+        String message = customerService.requestProvider(userId);
+        return Map.of("message", message);
+    }
+
 }
 
 
