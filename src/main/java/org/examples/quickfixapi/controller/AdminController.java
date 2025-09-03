052433b652dec7673d0f7a3eea2ee09e62388edd
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-03 15:19:45 +0530

    add get endpoint /prnding-requests to fetch pending provider requests

--- src/main/java/org/examples/quickfixapi/controller/AdminController.java
+++ src/main/java/org/examples/quickfixapi/controller/AdminController.java
@@ -1,9 +1,13 @@
 package org.examples.quickfixapi.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.examples.quickfixapi.entity.ProviderRequest;
 import org.examples.quickfixapi.service.AdminService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -12,5 +16,8 @@ public class AdminController {
 
     private final AdminService adminService;
 
-
+    @GetMapping("/pending-requests")
+    public List<ProviderRequest> getPendingRequests() {
+        return adminService.getPendingRequests();
+    }
 }
