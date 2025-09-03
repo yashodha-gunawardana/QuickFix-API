d8f4dc680a699ebaa8ef50997633e1e0e56c383a
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-03 14:38:32 +0530

    add jobDTO with validation for job details

--- src/main/java/org/examples/quickfixapi/dto/JobDTO.java
+++ src/main/java/org/examples/quickfixapi/dto/JobDTO.java
@@ -1,6 +1,27 @@
 package org.examples.quickfixapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class JobDTO {
 
+    @NotBlank(message = "Title is required")
+    private String title;
 
+    @NotBlank(message = "Category is required")
+    private String category;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @NotBlank(message = "Location is required")
+    private String location;
+
+    @NotBlank(message = "Budget must be positive")
+    private Double budget;
+
+    private String preferredDate;
+
+    private String preferredTime;
 }
