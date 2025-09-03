e93ef038390e6515d9c8538de9d2f06e186db98f
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-03 15:17:54 +0530

    add adminController class with adminService injection

--- src/main/java/org/examples/quickfixapi/controller/AdminController.java
+++ src/main/java/org/examples/quickfixapi/controller/AdminController.java
@@ -1,6 +1,16 @@
 package org.examples.quickfixapi.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.examples.quickfixapi.service.AdminService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final AdminService adminService;
 
 
 }
