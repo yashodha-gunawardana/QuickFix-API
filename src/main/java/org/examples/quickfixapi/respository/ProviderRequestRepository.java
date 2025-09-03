b6b62119feb62049a1cf7796f564b67d0518d696
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-03 14:53:57 +0530

    add providerRequestRepository for crud operations and custom existsByUserIdAndStatus method

--- src/main/java/org/examples/quickfixapi/respository/ProviderRequestRepository.java
+++ src/main/java/org/examples/quickfixapi/respository/ProviderRequestRepository.java
@@ -1,6 +1,10 @@
 package org.examples.quickfixapi.respository;
 
+import org.examples.quickfixapi.entity.ProviderRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ProviderRequestRepository {
-
+@Repository
+public interface ProviderRequestRepository  extends JpaRepository<ProviderRequest, Long> {
+    boolean existsByUserIdAndStatus(Long userId, String status);
 }
