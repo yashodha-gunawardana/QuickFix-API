15f9a7d31fa9a8e6e432baa840bb1310a504a066
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-04 18:28:23 +0530

    create NotificationService with constructor injection for UserRepository and NotificationRepository

--- src/main/java/org/examples/quickfixapi/service/NotificationService.java
+++ src/main/java/org/examples/quickfixapi/service/NotificationService.java
@@ -1,7 +1,21 @@
 package org.examples.quickfixapi.service;
 
+import org.examples.quickfixapi.respository.NotificationRepository;
+import org.examples.quickfixapi.respository.UserRepository;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class NotificationService {
+
+    private final UserRepository userRepository;
+    private final NotificationRepository notificationRepository;
+
+    public NotificationService(UserRepository userRepository, NotificationRepository notificationRepository) {
+        this.userRepository = userRepository;
+        this.notificationRepository = notificationRepository;
+    }
+
 
 
 
