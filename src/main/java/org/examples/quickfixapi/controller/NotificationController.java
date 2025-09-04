ef124ae6a4de375cdf1eabc19ef9cf5b2288b193
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-04 18:40:42 +0530

    add GET /user/{userId} endpoint to fetch unread notifications

--- src/main/java/org/examples/quickfixapi/controller/NotificationController.java
+++ src/main/java/org/examples/quickfixapi/controller/NotificationController.java
@@ -1,7 +1,10 @@
 package org.examples.quickfixapi.controller;
 
+import org.examples.quickfixapi.entity.Notification;
 import org.examples.quickfixapi.service.NotificationService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -14,5 +17,10 @@ public class NotificationController {
         this.notificationService = notificationService;
     }
 
+
+    @GetMapping("/user/{userId}")
+    public List<Notification> getUserNotifications(@PathVariable Long userId) {
+        return notificationService.getUnreadNotifications(userId);
+    }
 
 }
