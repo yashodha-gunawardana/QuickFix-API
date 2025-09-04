be73f2839284b6657e35bca055ee8a4d7abaac31
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-04 18:25:28 +0530

    add NotificationRepository with methods to fetch unread notifications, all notifications sorted by creation date, and count of unread notifications

--- src/main/java/org/examples/quickfixapi/respository/NotificationRepository.java
+++ src/main/java/org/examples/quickfixapi/respository/NotificationRepository.java
@@ -1,5 +1,15 @@
 package org.examples.quickfixapi.respository;
 
+import org.examples.quickfixapi.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface NotificationRepository {
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByUserIdAndIsReadFalse(Long userId);
+    List<Notification> findByUserIdOrderByCreatedAtDesc(Long userId);
+    int countByUserIdAndIsReadFalse(Long userId);
 }
