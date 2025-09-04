a83710f98f01da78f032e5a954fe2e00eebd1170
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-04 18:50:34 +0530

    converts each role in the roles set into a Spring Security authority (ROLE_<role>)

--- src/main/java/org/examples/quickfixapi/entity/User.java
+++ src/main/java/org/examples/quickfixapi/entity/User.java
@@ -42,7 +42,9 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                .toList();
     }
 
     @Override
