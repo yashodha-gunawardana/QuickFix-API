c5584a8c9fbfe47a2a4d3d4995ef057845f31443
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-04 18:39:30 +0530

    create NotificationController skeleton with constructor injection for NotificationService

--- src/main/java/org/examples/quickfixapi/controller/NotificationController.java
+++ src/main/java/org/examples/quickfixapi/controller/NotificationController.java
@@ -1,8 +1,18 @@
 package org.examples.quickfixapi.controller;
 
+import org.examples.quickfixapi.service.NotificationService;
+import org.springframework.web.bind.annotation.*;
 
+
+@RestController
+@RequestMapping("/api/notifications")
 public class NotificationController {
 
+    private final NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
 
 
 }
