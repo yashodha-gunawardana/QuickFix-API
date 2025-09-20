0e01b3a4b0728343c0656bf0a7860c440617f56b
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-20 21:31:09 +0530

    add method to fetch all pending provider requests

--- src/main/java/org/examples/quickfixapi/service/AdminService.java
+++ src/main/java/org/examples/quickfixapi/service/AdminService.java
@@ -1,8 +1,30 @@
 package org.examples.quickfixapi.service;
 
+import lombok.RequiredArgsConstructor;
+import org.examples.quickfixapi.dto.ProviderRequestDTO;
+import org.examples.quickfixapi.respository.JobRepository;
+import org.examples.quickfixapi.respository.ProviderRequestRepository;
+import org.examples.quickfixapi.respository.UserRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class AdminService {
 
+    private final ProviderRequestRepository providerRequestRepository;
+    private final UserRepository userRepository;
+    private final NotificationService notificationService;
+    private final JobRepository jobRepository;
 
+
+    public List<ProviderRequestDTO> getPendingRequests() {
+        return providerRequestRepository.findByStatus("PENDING")
+                .stream()
+                .map(this::converToDTO)
+                .collect(Collectors.toList());
+    }
 
 }
