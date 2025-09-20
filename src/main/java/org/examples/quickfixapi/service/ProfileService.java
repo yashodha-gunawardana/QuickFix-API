f66b84ceae99cf0e6f442827ef07cfe37d7ea63a
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-20 21:12:12 +0530

    add method to update customer profile with image upload

--- src/main/java/org/examples/quickfixapi/service/ProfileService.java
+++ src/main/java/org/examples/quickfixapi/service/ProfileService.java
@@ -2,17 +2,20 @@ package org.examples.quickfixapi.service;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.examples.quickfixapi.dto.CustomerProfileDTO;
 import org.examples.quickfixapi.entity.CustomerProfile;
 import org.examples.quickfixapi.entity.User;
 import org.examples.quickfixapi.respository.CustomerProfileRepository;
 import org.examples.quickfixapi.respository.ProviderProfileRepository;
 import org.examples.quickfixapi.respository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 
 @Service
@@ -53,6 +56,32 @@ public class ProfileService {
                     return customerProfileRepository.save(profile); // Save default profile
                 });
     }
+
+
+    // save customer profile
+    public CustomerProfile updateCustomerProfile(Long userId, CustomerProfileDTO customerProfileDTO, MultipartFile imageFile) throws IOException {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+        CustomerProfile customerProfile = customerProfileRepository.findByUserId(userId).orElse(CustomerProfile.builder().user(user).build());
+
+
+        customerProfile.setFirstName(customerProfileDTO.getFirstName());
+        customerProfile.setLastName(customerProfileDTO.getLastName());
+        customerProfile.setPhoneNo(customerProfileDTO.getPhoneNo());
+        customerProfile.setAddress(customerProfileDTO.getAddress());
+        customerProfile.setBio(customerProfileDTO.getBio());
+
+        // handle image upload
+        if (imageFile != null && !imageFile.isEmpty()) {
+            String fileName = userId + "_" + imageFile.getOriginalFilename();
+            Files.copy(imageFile.getInputStream(), rootLocation.resolve(fileName), StandardCopyOption.REPLACE_EXISTING);
+
+            user.setProfileImage(fileName); // save in user
+            customerProfile.setProfileImage(fileName); // save in customer profile
+            userRepository.save(user);
+        }
+        return customerProfileRepository.save(customerProfile);
+    }
+
 
 
 
