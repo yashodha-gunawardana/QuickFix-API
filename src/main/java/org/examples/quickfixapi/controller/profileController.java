1a34e69e0c76157f046a7018f766d957fcbdd1b6
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-20 21:23:41 +0530

    add endpoint to update customer profile with optional image upload

--- src/main/java/org/examples/quickfixapi/controller/profileController.java
+++ src/main/java/org/examples/quickfixapi/controller/profileController.java
@@ -7,10 +7,7 @@ import org.examples.quickfixapi.entity.CustomerProfile;
 import org.examples.quickfixapi.service.ProfileService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/profile")
@@ -30,6 +27,15 @@ public class profileController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(new ApiResponse(500, "Error fetching profile: " + e.getMessage(), null));
         }
+    }
+
+
+    @PutMapping(value = "/customer/{userId}", consumes = {"multipart/form-data"})
+    public ResponseEntity<ApiResponse> updateCustomerProfile(@PathVariable Long userId,
+                                                             @RequestPart("customerProfile") CustomerProfileDTO customerProfileDTO,
+                                                             @RequestPart(value = "image", required = false) MultipartFile image) throws IOException {
+        CustomerProfile customerProfile = profileService.updateCustomerProfile(userId, customerProfileDTO, image);
+        return ResponseEntity.ok(new ApiResponse(200, "Customer profile updated", customerProfile));
     }
 
 
