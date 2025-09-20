8e503a30357cb1cc8c1da579ef25b331f33eda37
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-09-20 21:09:52 +0530

    create uploads folder on startup

--- src/main/java/org/examples/quickfixapi/service/ProfileService.java
+++ src/main/java/org/examples/quickfixapi/service/ProfileService.java
@@ -1,6 +1,39 @@
 package org.examples.quickfixapi.service;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.examples.quickfixapi.respository.CustomerProfileRepository;
+import org.examples.quickfixapi.respository.ProviderProfileRepository;
+import org.examples.quickfixapi.respository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+@Service
+@RequiredArgsConstructor
 public class ProfileService {
+
+    private final CustomerProfileRepository customerProfileRepository;
+    private final ProviderProfileRepository providerProfileRepository;
+    private  final UserRepository userRepository;
+
+    // to store images
+    private final Path rootLocation = Paths.get("uploads");
+
+
+    // create folder if not exist
+    @PostConstruct
+    public void init() throws IOException {
+        if (!Files.exists(rootLocation)) {
+            Files.createDirectories(rootLocation);
+        }
+    }
+
+
 
 
 
